c8703d91bef189081d39495a6210d9ab9b55f7bc
Andy Kruta <andy.kruta@advisory.com>
2018-02-19 22:23:06 -0500

    Registered admin for Team model

--- Playbook/admin.py
+++ Playbook/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import SME
+from .models import SME, Team
 
 # Register your models here.
 class SMEAdmin(admin.ModelAdmin):
-    list_display = ("user_name")
+    list_display = ("username")
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("teamname")
+
+
 
 admin.site.register(SME)
+admin.site.register(Team)

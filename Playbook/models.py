d86ad8dcd3078b8f14df3d3f3192d24f72dd06b7
Andy Kruta <andy.kruta@advisory.com>
2018-02-19 22:26:12 -0500

    Added some of the teams model

--- Playbook/models.py
+++ Playbook/models.py
@@ -35,7 +35,7 @@ class SME(models.Model):
 class Team(models.Model):
     key = models.AutoField(primary_key=True)
     teamname = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("Team Name"))
-    teammanager = models.ForeignKey(to=SME, related_name="team", null=True, blank=True, verbose_name=_("Team Manager"))
+    teammanager = models.ForeignKey(to=SME, related_name="team", on_delete=models.CASCADE, null=True, blank=True, verbose_name=_("Team Manager"))
     smes = models.ManyToManyField("SME", related_name="team",verbose_name=_("Team Members"))
 
 

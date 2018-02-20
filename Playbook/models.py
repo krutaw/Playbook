8bedee0dc536948928f96eb806858b1dfe1ba0e0
Andy Kruta <andy.kruta@advisory.com>
2018-02-19 21:57:06 -0500

    Updated to remove underscores

--- Playbook/models.py
+++ Playbook/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class SME(models.Model):
     key = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=255, null=False, verbose_name=_("User name"))
-    email_address = models.EmailField(max_length=255, null=False, verbose_name=_("Email Address"))
-    phone_regex = RegexValidator(regex=r'^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$')
-    phone_number = models.CharField(max_length=20, null=False,blank=False,validators=[phone_regex])
-    given_name = models.CharField(max_length=255, null=False, blank=False)
+    username = models.CharField(max_length=255, null=False, verbose_name=_("User name"))
+    emailaddress = models.EmailField(max_length=255, null=False, verbose_name=_("Email Address"))
+    phoneregex = RegexValidator(regex=r'^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$')
+    phonenumber = models.CharField(max_length=20, null=False,blank=False,validators=[phone_regex])
+    givenname = models.CharField(max_length=255, null=False, blank=False)
     surname = models.CharField(max_length=255, null=False, blank=False)
     ctime = models.DateTimeField(auto_now_add=True)
     mtime = models.DateTimeField(auto_now=True)

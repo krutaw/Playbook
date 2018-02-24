a17a7cbfff835ac76c65dd91edc5f14566f74f4b
Andy Kruta <andy.kruta@advisory.com>
2018-02-24 17:39:44 -0500

    Updated to include Serializers for the remaining models

--- Playbook/serializers.py
+++ Playbook/serializers.py
@@ -2,7 +2,7 @@
 Serializer module for the API
 '''
 from rest_framework import serializers
-from Playbook.models import SME
+from Playbook.models import SME, Team, Calendar, Schedule, ScheduleRotation, Actions, Play, PlayBook
 # added the models below because I'm lazy but have them commented out for now to allow linter to pass
 # , Team, Calendar, Schedule, RecurRule, ScheduleRotation
 # from Playbook.models import Certificate, Actions, Play, PlayBook
@@ -17,4 +17,88 @@ class SMESerializer(serializers.ModelSerializer):
         Meta class for the serializer.
         '''
         model = SME
+        fields = '__all__'
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    '''
+    Team Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = Team
+        fields = '__all__'
+
+
+class CalendarSerializer(serializers.ModelSerializer):
+    '''
+    Calendar Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = Calendar
+        fields = '__all__'
+
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    '''
+    Schedule Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = Schedule
+        fields = '__all__'
+
+
+class ScheduleRotationSerializer(serializers.ModelSerializer):
+    '''
+    ScheduleRotation Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = ScheduleRotation
+        fields = '__all__'
+
+
+class ActionsSerializer(serializers.ModelSerializer):
+    '''
+    Actions Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = Actions
+        fields = '__all__'
+
+
+class PlaySerializer(serializers.ModelSerializer):
+    '''
+    Play Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = Play
+        fields = '__all__'
+
+
+class PlayBookSerializer(serializers.ModelSerializer):
+    '''
+    PlayBook Serializer
+    '''
+    class Meta:
+        '''
+        Meta class for the serializer.
+        '''
+        model = PlayBook
         fields = '__all__'
